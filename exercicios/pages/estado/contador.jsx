72072f5e7cd99dd35dbdedfff5e980860bb55934
igregorioneto <igregoriodoneto@outlook.com>
2024-12-23 08:54:37 -0300

    feat: Contador Display

---
+++ exercicios/pages/estado/contador.jsx
@@ -0,0 +1,25 @@
+import ContadorDisplay from "@/components/ContadorDisplay";
+import { useState } from "react";
+
+export default function contador() {
+    const [numero, setNumero] = useState(0);
+
+    const dec = () => setNumero(numero - 1);
+    const inc = () => setNumero(numero + 1)
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <h1>Contador</h1>
+            <ContadorDisplay numero={numero} />
+            <div>
+                <button onClick={dec}>-</button>
+                <button onClick={inc}>+</button>
+            </div>
+        </div>
+    )
+}
